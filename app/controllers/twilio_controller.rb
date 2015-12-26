8e3c2c9cbf46cdead5e688c507582bf8e0b20ab1
Phil Battos <philbattos@gmail.com>
2015-12-26 13:30:41 -0700

    Format incoming params for received SMS messages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,7 +3,6 @@ module Twilio
 
     def create
       puts params
-      # sms_parameters.to_a.map {|x| sms_parameters[x[0].to_s.underscore.to_sym] = sms_parameters.delete(x[0])}
       @sms = SMS.new(formatted_params)
       @sms.save
 
@@ -40,7 +39,23 @@ module Twilio
       {
         to_country:       sms_parameters[:ToCountry],
         to_state:         sms_parameters[:ToState],
-        sms_message_sid:  sms_parameters[:SmsMessageSid]
+        sms_message_sid:  sms_parameters[:SmsMessageSid],
+        num_media:        sms_parameters[:NumMedia],
+        to_city:          sms_parameters[:ToCity],
+        from_zip:         sms_parameters[:FromZip],
+        sms_sid:          sms_parameters[:SmsSid],
+        from_state:       sms_parameters[:FromState],
+        sms_status:       sms_parameters[:SmsStatus],
+        from_city:        sms_parameters[:FromCity],
+        body:             sms_parameters[:Body],
+        from_country:     sms_parameters[:FromCountry],
+        to:               sms_parameters[:To],
+        to_zip:           sms_parameters[:ToZip],
+        num_segments:     sms_parameters[:NumSegments],
+        message_sid:      sms_parameters[:MessageSid],
+        account_sid:      sms_parameters[:AccountSid],
+        from:             sms_parameters[:From],
+        api_version:      sms_parameters[:ApiVersion]
       }
     end
 
