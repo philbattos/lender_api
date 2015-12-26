ba9520ab4d5051ef8e7ea4d409eaa881729bc146
Phil Battos <philbattos@gmail.com>
2015-12-26 10:48:34 -0700

    Update controller actions for receiving SMS messages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,19 +1,21 @@
-class TwilioController < ApplicationController
+module Twilio
+  class SMSController < ApplicationController
 
-  def sms
-    puts params
-    # @sms = SMS.new(body: params[:Body], from: params[:From])
-    # @sms.save
+    def sms
+      puts params
+      # @sms = SMS.new(body: params[:Body], from: params[:From])
+      # @sms.save
 
-    render xml: "<Response><Message>Test response from Twilio#sms.</Message></Response>"
+      render xml: "<Response><Message>Test response from Twilio#sms.</Message></Response>"
+    end
+
+    def create
+      puts params
+      # @sms = SMS.new(body: params[:Body], from: params[:From])
+      # @sms.save
+
+      render xml: "<Response><Message>Test response from Twilio#create.</Message></Response>"
+    end
+
   end
-
-  def create
-    puts params
-    # @sms = SMS.new(body: params[:Body], from: params[:From])
-    # @sms.save
-
-    render xml: "<Response><Message>Test response from Twilio#create.</Message></Response>"
-  end
-
 end
