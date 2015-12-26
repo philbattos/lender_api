d395c4672389688b2259259d284fb4be182acb5b
Phil Battos <philbattos@gmail.com>
2015-12-26 13:24:28 -0700

    Format incoming params for received SMS messages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,8 +3,8 @@ module Twilio
 
     def create
       puts params
-      sms_parameters.to_a.map {|x| sms_parameters[x[0].to_s.underscore.to_sym] = sms_parameters.delete(x[0])}
-      @sms = SMS.new(sms_parameters)
+      # sms_parameters.to_a.map {|x| sms_parameters[x[0].to_s.underscore.to_sym] = sms_parameters.delete(x[0])}
+      @sms = SMS.new(formatted_params)
       @sms.save
 
       render xml: "<Response><Message>Test response from Twilio#create.</Message></Response>"
@@ -14,28 +14,34 @@ module Twilio
     private
   #=================================================
     def sms_parameters
-      params.permit(:to_country,
-                    :to_state,
-                    :sms_message_sid,
-                    :num_media,
-                    'from_state',
-                    'from_city'
-                    # :ToCity,
-                    # :FromZip,
-                    # :SmsSid,
-                    # :FromState,
-                    # :SmsStatus,
-                    # :FromCity,
-                    # :Body,
-                    # :FromCountry,
-                    # :To,
-                    # :ToZip,
-                    # :NumSegments,
-                    # :MessageSid,
-                    # :AccountSid,
-                    # :From,
-                    # :ApiVersion
+      params.permit(:ToCountry,
+                    :ToState,
+                    :SmsMessageSid,
+                    :NumMedia,
+                    :ToCity,
+                    :FromZip,
+                    :SmsSid,
+                    :FromState,
+                    :SmsStatus,
+                    :FromCity,
+                    :Body,
+                    :FromCountry,
+                    :To,
+                    :ToZip,
+                    :NumSegments,
+                    :MessageSid,
+                    :AccountSid,
+                    :From,
+                    :ApiVersion
                     )
+    end
+
+    def formatted_params
+      {
+        to_country:       sms_parameters[:ToCountry],
+        to_state:         sms_parameters[:ToState],
+        sms_message_sid:  sms_parameters[:SmsMessageSid]
+      }
     end
 
   end
