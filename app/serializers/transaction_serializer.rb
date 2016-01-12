2786b7b500211a0dd1c580129c526fc8fac65f59
Phil Battos <philbattos@gmail.com>
2016-01-12 11:48:26 -0700

    Update transaction attributes sent to client

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,3 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :peer_id, :amount, :terms
+  attributes :id, :user_id, :peer_id, :amount, :terms, :aasm_state, :start_date, :end_date
 end
