23540c3e2c3351511235cf17dda7469e6f15e18c
Phil Battos <philbattos@gmail.com>
2016-01-29 01:12:01 -0700

    Debug sessions controller

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -4,10 +4,15 @@ class User::SessionsController < Devise::SessionsController
   def create
     sign_out(:user) if current_user
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    # set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
+    puts "resource: #{resource}"
+    puts "resource_name: #{resource_name}"
+    puts "location: #{location}"
+    puts "block_given?: #{block_given?}"
+    puts "current_user: #{current_user}"
     yield resource if block_given?
     respond_with resource, location
-    after_sign_in_path_for(resource)
+    # after_sign_in_path_for(resource)
   end
 end
