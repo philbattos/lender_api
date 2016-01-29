2cd331983a4c4cc97399aeafd62879b60940ec46
Phil Battos <philbattos@gmail.com>
2016-01-29 00:03:02 -0700

    Fix sessions controller

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -7,6 +7,7 @@ class User::SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location after_sign_in_path_for(resource)
+    respond_with resource, location
+    after_sign_in_path_for(resource)
   end
 end
