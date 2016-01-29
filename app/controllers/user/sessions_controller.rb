7bece535b29e36d51b6ce09cd0bc4280626c464e
Phil Battos <philbattos@gmail.com>
2016-01-29 01:14:27 -0700

    Debug sessions controller

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -6,13 +6,13 @@ class User::SessionsController < Devise::SessionsController
     self.resource = warden.authenticate!(auth_options)
     # set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
-    puts "resource: #{resource}"
+    puts "resource: #{resource.inspect}"
     puts "resource_name: #{resource_name}"
     puts "location: #{location}"
     puts "block_given?: #{block_given?}"
-    puts "current_user: #{current_user}"
+    puts "current_user: #{current_user.inspect}"
     yield resource if block_given?
-    respond_with resource, location
+    respond_with resource
     # after_sign_in_path_for(resource)
   end
 end
