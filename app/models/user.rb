850e8fc175f59f84828548f0d78758832fe181a9
Phil Battos <philbattos@gmail.com>
2016-01-29 21:24:06 -0700

    Remove validations from Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  #-------------------------------------------------
+  #    Authentication with Devise
+  #-------------------------------------------------
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :trackable,
+          # :validatable,
+          # :confirmable,
+          # :lockable,
+          # :timeoutable,
+          # :omniauthable
+
   #-------------------------------------------------
   #    Associations
   #-------------------------------------------------
