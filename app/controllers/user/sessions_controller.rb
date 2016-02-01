ff06a794f63798245c66ab61d0f6a8398947cb67
Phil Battos <philbattos@gmail.com>
2016-01-31 18:29:37 -0700

    Debug current user

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -2,9 +2,11 @@ class User::SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
+    puts "SESSION-1 current_user: #{current_user.id}"
     sign_out(:user) if current_user
     self.resource = warden.authenticate!(auth_options)
     sign_in(resource_name, resource)
+    puts "SESSION-2 current_user: #{current_user.id}"
     yield resource if block_given?
     # respond_with resource
     render json: resource
