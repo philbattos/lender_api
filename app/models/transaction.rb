4223c43053451d5f0521f7fa2e08e6c8e9198fc1
Phil Battos <philbattos@gmail.com>
2016-02-17 02:39:23 -0700

    Update interest calculation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -65,10 +65,10 @@ class Transaction < ActiveRecord::Base
   end
 
   def accrued_interest
-    apr         = terms.to_f / 100 # assumes that '.terms' is a number between 1-100
+    daily_apr   = terms.to_f / 100 # assumes that '.terms' is a number between 1-100
     start       = start_date || created_at
     days        = (Time.current.to_date - start.to_date).to_i
-    daily_rate  = apr * days / 365
+    daily_rate  = daily_apr / 365
 
     @accrued_interest = (daily_rate * days * amount).round(2)
   end
